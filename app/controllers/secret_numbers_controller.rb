5b473f36cbda65f242c765ab79731f2eaa795e26
Kevin Lee <kevinleeme@gmail.com>
2014-02-18 19:33:40 -0800

    secret number game

---
+++ app/controllers/secret_numbers_controller.rb
@@ -0,0 +1,12 @@
+class SecretNumbersController < ApplicationController
+
+def new
+
+end
+
+def show
+	@secret_number = rand(1..5)
+	@chosen_number = params[:id].to_i
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Games::Application.routes.draw do
+
+  root to: 'games#index'
+
+  resources :games
+
+  resources :secret_numbers, only: [:new, :show]
+
+  # get 'games' => 'games#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
